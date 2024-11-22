1fc476a90eba55ba5835866ac636a53c8863d819
Saad Monem <smonem@umich.edu>
2024-11-22 00:39:08 -0500

    Added truncated dataset for testing purposes, also shortened some code

--- app/model.py
+++ app/model.py
@@ -3,21 +3,9 @@
 class Banana:
 
     def __init__(self, **kwargs):
-        self.variety = kwargs.get("variety")
-        self.region = kwargs.get("region")
-        self.quality_score = kwargs.get("quality_score")
-        self.quality_category = kwargs.get("quality_category")
-        self.ripeness_index = kwargs.get("ripeness_index")
-        self.ripeness_category = kwargs.get("ripeness_category")
-        self.sugar_content_brix = kwargs.get("sugar_content_brix")
-        self.firmness_kgf = kwargs.get("firmness_kgf")
-        self.length_cm = kwargs.get("length_cm")
-        self.weight_g = kwargs.get("weight_g")
-        self.harvest_date = kwargs.get("harvest_date")
-        self.tree_age_years = kwargs.get("tree_age_years")
-        self.altitude_m = kwargs.get("altitude_m")
-        self.rainfall_mm = kwargs.get("rainfall_mm")
-        self.soil_nitrogen_ppm = kwargs.get("soil_nitrogen_ppm")
+        # Copies keys and values from dict passed in as attributes
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def __str__(self):
         # TODO: Extend string representation with more data
