062b6e7f6b053ee73cda4eb35695035387eea144
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2024-12-05 16:25:34 -0500

    os import

--- app/view.py
+++ app/view.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import numpy as np
 import re
 import matplotlib.pyplot as mplb
+import os
 
 
 ################################################################################################################### 
